d9405ade1cd0db922cc44d444ca4edf0974b96ad
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2019-04-09 13:13:06 +0200

    compression update
    added a compression update to improve loading times

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function(){
     var y= this.window.pageYOffset;
-    this.document.getElementById("parallax").style.backgroundPositionY= (y* -0.5)+"px";
+    this.document.getElementById("").style.backgroundPositionY= (y*-0.5)+"px";
 });
 
 AOS.init({
