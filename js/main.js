6da58d7f3df17dde371bf8382da207b0173d1df1
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2019-04-09 12:16:17 +0200

    new additions
    prior to this update, multiple changes and additions have been made to this cv but due to internet connectivity issues it had to be stored offline in the meantime

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", function(){
     var y= this.window.pageYOffset;
-    this.document.getElementById("").style.backgroundPositionY= (y*-0.5)+"px";
+    this.document.getElementById("parallax").style.backgroundPositionY= (y* -0.5)+"px";
 });
 
 AOS.init({
-    duration: 2000,
+    duration: 1200
 });
 
 
